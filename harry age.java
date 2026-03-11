83b21e00ce5a517824d0588280fc7eb12e947206
vysakh00 <vysakhmurali999@gmail.com>
2026-03-11 08:48:05 +0530

    Create harry age.java

---
+++ harry age.java
@@ -0,0 +1,9 @@
+public class a
+{
+    public static void main(String [] args)
+    {
+            //1
+            int age=2024-2000;
+            System.out.println("Harry's age in 2024 is "+age);
+   }
+}
