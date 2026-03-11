9e1a83ce45ebaf68c39b682a9697da6d7ab78925
vysakh00 <vysakhmurali999@gmail.com>
2026-03-11 08:47:12 +0530

    Create cost.java

---
+++ cost.java
@@ -0,0 +1,12 @@
+public class a
+{
+    public static void main(String [] args)
+    {
+            //4
+            int cp=129,sp=191;
+            int p=sp-cp;
+            double pp=p*100/cp;
+            System.out.println("the cost in inr is "+cp+" and the selling price is "+sp);
+            System.out.println("the profit in inr is "+p+" and the profit percentage is "+pp);
+   }
+}
