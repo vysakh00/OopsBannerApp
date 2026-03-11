e7ea4a8dd2744f2407aa4e4c26bf6cd981268311
vysakh00 <vysakhmurali999@gmail.com>
2026-03-11 08:46:23 +0530

    Create avg marks.java

---
+++ avg marks.java
@@ -0,0 +1,11 @@
+public class a
+{
+    public static void main(String [] args)
+    {
+            //2
+            int maths=94;
+            int phy=95;
+            int chem=96;
+            System.out.println("Sam's avg marks="+(maths+chem+phy)/3);
+    }
+}
