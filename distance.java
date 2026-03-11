c900a9905339a3d70588bceb769b1211f6732532
vysakh00 <vysakhmurali999@gmail.com>
2026-03-11 08:47:41 +0530

    Create distance.java

---
+++ distance.java
@@ -0,0 +1,11 @@
+public class a
+{
+    public static void main(String [] args)
+    {
+     
+            //3
+            double km =10.8;
+            double miles=1.6*km;
+            System.out.println("the distance "+km+" km in miles is "+miles);
+    }
+}
