afdff5f62324e37f708106d473b21bb944121dbb
vysakh00 <vysakhmurali999@gmail.com>
2026-03-11 08:48:36 +0530

    Create pen.java

---
+++ pen.java
@@ -0,0 +1,10 @@
+public class a
+{
+    public static void main(String [] args)
+    {
+
+            //5
+            int pen=14;
+            System.out.println("number of  pen each student will get "+pen/3+" remaining pen are "+pen%3);
+    }
+}
